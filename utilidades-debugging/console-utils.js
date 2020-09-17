247652f14faa422cabcce66f6d23f957cce5335b
cristianiniguez <felizpc1@gmail.com>
2020-09-17 15:14:28 -0400

    Consola, utilidades y debugging

---
+++ utilidades-debugging/console-utils.js
@@ -0,0 +1,17 @@
+// %s
+// %d
+// %j
+// console.log('Un %s y un %s', 'perro', 'gato')
+
+// console.info('hello world')
+// console.warn('hello world')
+
+// console.assert(42 == '42')
+// console.assert(42 === '42')
+
+// console.trace('hello')
+
+const util = require('util')
+const debuglog = util.debuglog('foo')
+
+debuglog('hello from foo')
